35f15e5f2ebe6ccdaf4599f46108b6a3ea55c61d
Ryan <ryan@rmiller.dev>
2020-01-03 12:00:29 -0500

    done

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -1,18 +1,37 @@
 package rocks.zipcode.io.quiz4.generics;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class GenericUtils {
-    public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(Set<_> originalSet) {
-        return null;
+    public static <T extends Comparable<T>> Iterable<? extends Iterable<T>> powerSet(Set<T> originalSet) {
+        Set<Set<T>> powerSet = new LinkedHashSet<>();
+
+        if (originalSet.isEmpty()) {
+            powerSet.add(new LinkedHashSet<T>());
+            return powerSet;
+        }
+
+        List<T> originalList = new ArrayList<>(originalSet);
+        T head = originalList.get(0);
+        Set<T> rest = new LinkedHashSet<T>(originalList.subList(1, originalList.size()));
+        for (Iterable<T> i : powerSet( rest)) {
+            Set<T> newSet = new HashSet<T>();
+            for (T t : i) {
+                newSet.add(t);
+            }
+            powerSet.add(newSet);
+
+            newSet.add(head);
+            powerSet.add(newSet);
+        }
+        return powerSet;
     }
 
-    public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(_... originalSet) {
+
+    public static <T extends Comparable<T>> Iterable<? extends Iterable<T>> powerSet(T... originalSet) {
         return powerSet(new TreeSet<>(Arrays.asList(originalSet)));
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
@@ -1,27 +1,43 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class MyStack<SomeType> {
+public class MyStack<SomeType> implements Iterable<SomeType> {
 
+    List<SomeType> stack = new ArrayList<>();
 
-    public MyStack() {
-
-    }
+    public MyStack() { }
 
     public Boolean isEmpty() {
-        return null;
+        return stack.size() == 0;
     }
 
     public void push(SomeType i) {
+        stack.add(i);
     }
 
     public SomeType peek() {
-        return null;
+        if(isEmpty()) {
+            return null;
+        }
+        return stack.get(stack.size() - 1);
     }
 
     public SomeType pop() {
-        return null;
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    @Override
+    public Iterator<SomeType> iterator() {
+        return stack.iterator();
     }
 }
